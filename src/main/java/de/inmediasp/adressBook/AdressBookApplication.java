00c2d45791b5f8863e1acabcfd4ad23252a83d65
amesbah <mesbah@inmedipsp.de>
2021-04-23 10:06:42 +0200

    Spring Starter

---
+++ src/main/java/de/inmediasp/adressBook/AdressBookApplication.java
@@ -0,0 +1,14 @@
+package de.inmediasp.adressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdressBookApplication.class, args);
+
+	}
+
+}
