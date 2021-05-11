11b8f0ad582a3d92db21b809ea5f6b97255dfbcf
amesbah <mesbah@inmedipsp.de>
2021-05-11 11:44:31 +0200

    Costume Client  Authorization and Encode password of Client and give him a special role

--- src/main/java/de/inmediasp/adressBook/Security/ApplicationConfigrationSecurity.java
+++ src/main/java/de/inmediasp/adressBook/Security/ApplicationConfigrationSecurity.java
@@ -1,13 +1,27 @@
 package de.inmediasp.adressBook.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationConfigrationSecurity extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationConfigrationSecurity(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,5 +31,16 @@ public class ApplicationConfigrationSecurity extends WebSecurityConfigurerAdapte
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user=User.builder()
+                .username("ahmed")
+                .password(passwordEncoder.encode("123456"))
+                .roles("contacts")
+                .build();
+        return  new InMemoryUserDetailsManager(user);
     }
 }
